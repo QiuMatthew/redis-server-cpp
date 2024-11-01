27ec05f40044182510f9b50f2708b9424723082d
QiuMatthew <uzenkyu@gmail.com>
2024-11-01 15:57:19 +0900

    respond to multiple pings

--- src/Server.cpp
+++ src/Server.cpp
@@ -9,16 +9,32 @@
 #include <iostream>
 #include <string>
 
+std::vector<char> receive_message_until_delimiter(int client_fd,
+												  char delimiter) {
+	std::vector<char> message;
+	while (true) {
+		std::vector<char> buffer(1);
+		ssize_t bytes_received = recv(client_fd, buffer.data(), 1, 0);
+		if (bytes_received == -1) {
+			std::cerr << "recv failed\n";
+			exit(1);
+		}
+		if (bytes_received == 0) {
+			std::cout << "Client disconnected\n";
+			break;
+		}
+		if (buffer[0] == delimiter) {
+			break;
+		}
+		message.push_back(buffer[0]);
+	}
+	return message;
+}
+
 int main(int argc, char **argv) {
 	// Flush after every std::cout / std::cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
-
-	// You can use print statements as follows for debugging, they'll be visible
-	// when running tests.
-	std::cout << "Logs from your program will appear here!\n";
-
-	// Uncomment this block to pass the first stage
 
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
@@ -62,11 +78,11 @@ int main(int argc, char **argv) {
 	std::cout << "Client connected\n";
 
 	while (true) {
-		char *request_msg[100];
-		if (recv(client_fd, (void *)request_msg, 100, 0) == -1) {
-			std::cerr << "receive message failed\n";
-			return 1;
-		}
+		std::string request_msg_str =
+			receive_message_until_delimiter(client_fd, '\n').data();
+		std::cout << "Received message length: " << request_msg_str.length()
+				  << std::endl;
+		std::cout << "Received message: " << request_msg_str << std::endl;
 		const char *response_to_ping = "+PONG\r\n";
 		if (send(client_fd, (const void *)response_to_ping,
 				 strlen(response_to_ping), 0) == -1) {
