c641d9ed40d3c8b1e4d2bdcc2af697b6b47e4437
QiuMatthew <uzenkyu@gmail.com>
2024-11-01 16:39:17 +0900

    respond to multiple pings

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,28 +10,6 @@
 #include <string>
 #include <vector>
 
-std::vector<char> receive_message_until_delimiter(int client_fd,
-												  char delimiter) {
-	std::vector<char> message;
-	while (true) {
-		std::vector<char> buffer(1);
-		ssize_t bytes_received = recv(client_fd, buffer.data(), 1, 0);
-		if (bytes_received == -1) {
-			std::cerr << "recv failed\n";
-			exit(1);
-		}
-		if (bytes_received == 0) {
-			std::cout << "Client disconnected\n";
-			break;
-		}
-		if (buffer[0] == delimiter) {
-			break;
-		}
-		message.push_back(buffer[0]);
-	}
-	return message;
-}
-
 int main(int argc, char **argv) {
 	// Flush after every std::cout / std::cerr
 	std::cout << std::unitbuf;
@@ -90,16 +68,21 @@ int main(int argc, char **argv) {
 			std::cout << "Client disconnected\n";
 			break;
 		}
-		// std::cout << "Received message length: " << request_msg_str.length()
-		// 		  << std::endl;
-		// std::cout << "Received message: " << request_msg_str << std::endl;
-		const char *response_to_ping = "+PONG\r\n";
-		if (send(client_fd, (const void *)response_to_ping,
-				 strlen(response_to_ping), 0) == -1) {
-			std::cerr << "send message failed\n";
-			return 1;
+		std::cout << "Received message length: " << bytes_received << std::endl;
+		std::string request_msg_str(request_msg.begin(),
+									request_msg.begin() + bytes_received);
+		std::cout << "Received message: " << request_msg_str << std::endl;
+		if (request_msg_str == "*1\r\n$4\r\nPING\r\n" ||
+			request_msg_str == "*1\r\n$4\r\nping\r\n") {
+			const char *response_to_ping = "+PONG\r\n";
+			if (send(client_fd, (const void *)response_to_ping,
+					 strlen(response_to_ping), 0) == -1) {
+				std::cerr << "send message failed\n";
+				return 1;
+			}
+			std::cout << "Sent PONG\n";
 		}
-		std::cout << "Sent PONG\n";
+		std::cout << "======================================================\n";
 	}
 
 	close(server_fd);
