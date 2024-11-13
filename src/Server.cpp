4753d69ccc213befda8a3360e7192a33356866df
QiuMatthew <uzenkyu@gmail.com>
2024-11-13 17:42:29 +0900

    delete redundant function

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,105 +10,15 @@
 #include <string>
 #include <thread>
 #include <unordered_map>
-#include <vector>
 
 #include "client_handler.h"
 #include "network_utils.h"
-#include "redis_command.h"
 #include "server.h"
 
-void handle_client(int client_fd) {
-	std::unordered_map<std::string, std::string> data = {};
-	while (true) {
-		// get the request message
-		std::vector<char> request_msg(100);
-		ssize_t bytes_received =
-			recv(client_fd, request_msg.data(), request_msg.size(), 0);
-		if (bytes_received == -1) {
-			std::cerr << "recv failed\n";
-			exit(1);
-		}
-		if (bytes_received == 0) {
-			std::cout << "Client disconnected\n";
-			return;
-		}
-		std::cout << "Received message length: " << bytes_received << std::endl;
-		std::string request_msg_str(request_msg.begin(),
-									request_msg.begin() + bytes_received);
-		std::cout << "Received message: " << request_msg_str << std::endl;
-
-		// Parse the request message
-		RedisCommand request_command(request_msg_str);
-
-		// Handle the request
-		if (request_command.get_command_type() == "PING") {
-			// Handle PING command
-			const char *response_to_ping = "+PONG\r\n";
-			if (send(client_fd, response_to_ping, strlen(response_to_ping),
-					 0) == -1) {
-				std::cerr << "send message failed\n";
-				exit(1);
-			}
-			std::cout << "Sent PONG\n";
-		} else if (request_command.get_command_type() == "ECHO") {
-			// Handle ECHO command
-			std::string response_to_echo_str =
-				"+" + request_command.get_command_args()[0] + "\r\n";
-			const char *response_to_echo = response_to_echo_str.c_str();
-			if (send(client_fd, response_to_echo, strlen(response_to_echo),
-					 0) == -1) {
-				std::cerr << "send message failed\n";
-				exit(1);
-			}
-			std::cout << "Sent ECHO\n";
-		} else if (request_command.get_command_type() == "SET") {
-			// Handle SET command
-			// Add the key-value pair to the dictionary
-			if (data.find(request_command.get_command_args()[0]) ==
-				data.end()) {
-				data.insert({request_command.get_command_args()[0],
-							 request_command.get_command_args()[1]});
-			} else {
-				data[request_command.get_command_args()[0]] =
-					request_command.get_command_args()[1];
-			}
-
-			// Send the response
-			std::string response_to_set_str = "+OK\r\n";
-			const char *response_to_set = response_to_set_str.c_str();
-			if (send(client_fd, response_to_set, strlen(response_to_set), 0) ==
-				-1) {
-				std::cerr << "send message failed\n";
-				exit(1);
-			}
-			std::cout << "Sent OK\n";
-		} else if (request_command.get_command_type() == "GET") {
-			// Handle GET command
-			// Get the value from the dictionary
-			std::string response_to_get_str;
-			if (data.find(request_command.get_command_args()[0]) ==
-				data.end()) {
-				response_to_get_str = "$-1\r\n";
-			} else {
-				std::string value = data[request_command.get_command_args()[0]];
-				response_to_get_str = "$" + std::to_string(value.size()) +
-									  "\r\n" + value + "\r\n";
-			}
-			const char *response_to_get = response_to_get_str.c_str();
-			std::cout << "Response to GET: " << response_to_get << std::endl;
-			if (send(client_fd, response_to_get, strlen(response_to_get), 0) ==
-				-1) {
-				std::cerr << "send message failed\n";
-				exit(1);
-			}
-			std::cout << "Sent GET response\n";
-		}
-	}
-	return;
-}
-
-Server::Server(int port) {
+Server::Server(int port, std::unordered_map<std::string, std::string> args) {
+	// store the config
 	this->port = port;
+	this->config = args;
 
 	// create a socket
 	this->server_fd = create_socket();
@@ -159,7 +69,21 @@ int main(int argc, char **argv) {
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
-	Server server(6379);
+	// parse args, the program will be run as ./server --dir <path_to_directory>
+	// --dbfilename <dbfilename>
+	std::string dir = "";
+	std::string dbfilename = "";
+	for (int i = 1; i < argc; i++) {
+		if (std::string(argv[i]) == "--dir") {
+			dir = argv[i + 1];
+		} else if (std::string(argv[i]) == "--dbfilename") {
+			dbfilename = argv[i + 1];
+		}
+	}
+	std::unordered_map<std::string, std::string> args = {
+		{"dir", dir}, {"dbfilename", dbfilename}};
+
+	Server server(6379, args);
 	server.start();
 	server.stop();
 
--- src/server.h
+++ src/server.h
@@ -1,15 +1,18 @@
 #pragma once
 #include <netinet/in.h>
 
+#include <unordered_map>
+
 class Server {
    public:
-	Server(int port);
+	Server(int port, std::unordered_map<std::string, std::string> args);
 	void start();
 	void stop();
 
    private:
 	int port;
 	int server_fd;
+	std::unordered_map<std::string, std::string> config;
 	struct sockaddr_in server_addr;
 	void accept_client();
 };
